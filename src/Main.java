394a7792e6d039832ede786630a6226b0d5e8065
Luan Pimenta <luan.pimenta@academico.ifpb.edu.br>
2024-10-23 11:34:28 -0300

    privado, publico, getters e setters

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import com.luanpimenta.mymovie.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie meuFilme = new Movie();
-        meuFilme.name = "Toy Story";
-        meuFilme.durationInMinutes = 90;
-        meuFilme.includesOnPlan = true;
-        meuFilme.ratingSum = 9;
-        meuFilme.ratingTotal = 1;
-        meuFilme.releaseYear = 1995;
+        meuFilme.setName("Toy Story");
+        meuFilme.setDurationInMinutes(90);
+        meuFilme.setincludesOnPlan(true);
+        meuFilme.setReleaseYear(1995);
 
-        meuFilme.showTechnicalDataSheet();
         meuFilme.rate(10);
         meuFilme.rate(5);
         meuFilme.rate(6);
+        System.out.println("Total de avaliações: " + meuFilme.getRatingTotal());
         meuFilme.showTechnicalDataSheet();
+
+        meuFilme.setReleaseYear(2025);
+        System.out.println(meuFilme.getReleaseYear());
     }
 }
---
+++ src/com/luanpimenta/mymovie/model/Movie.java
@@ -0,0 +1,77 @@
+package com.luanpimenta.mymovie.model;
+
+import java.time.LocalDate;
+
+public class Movie {
+    private String name;
+    private int releaseYear;
+    private boolean includesOnPlan;
+    private double ratingSum;
+    private int ratingTotal;
+    private int durationInMinutes;
+
+    public void showTechnicalDataSheet(){
+        System.out.printf("""
+                Nome: %s
+                Data lançamento: %d
+                Incluso: %b
+                Média avaliações: %.1f
+                Duração (minutos): %d
+                """, name, releaseYear, includesOnPlan, ratingAverage(), durationInMinutes);
+    }
+
+    public int getRatingTotal(){
+        return ratingTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReleaseYear() {
+        int currentYear = LocalDate.now().getYear();
+        if ( releaseYear <= currentYear && releaseYear > 1800){
+            return String.valueOf(releaseYear);
+        }else{
+            return "Ano inválido";
+        }
+    }
+
+    public boolean isIncludesOnPlan() {
+        return includesOnPlan;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes){
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public void setincludesOnPlan(boolean includesOnPlan){
+        this.includesOnPlan = includesOnPlan;
+    }
+
+    public void setReleaseYear(int releaseYear){
+        int currentYear = LocalDate.now().getYear();
+        if ( releaseYear <= currentYear && releaseYear > 1800){
+            this.releaseYear = releaseYear;
+        }else{
+            System.out.println("Ano inválido!");
+        }
+    }
+
+    public void rate(double number){
+        ratingSum += number;
+        ratingTotal++;
+    }
+
+    public double ratingAverage(){
+        return ratingSum / ratingTotal;
+    }
+}
